a2023f83bb4e91a40442f6d2a3a27275dd004f19
megat69 <assassincraft218@gmail.com>
2022-09-28 09:53:40 +0200

    Added syntax highlighting for docstring

--- docstring.py
+++ docstring.py
@@ -7,15 +7,24 @@ class DocstringPlugin(Plugin):
 	"""
 	def __init__(self, app):
 		super().__init__(app)
+		self.docstring_components = ("precond", "data", "result", "desc", "vars")
 		self.add_command("d", self.add_docstring, "Docstring")
-
+		self.app.color_control_flow["instruction"] = (
+			*self.app.color_control_flow["instruction"],
+			*self.docstring_components
+		)
 
 	def add_docstring(self):
 		"""
-		Adds the docstring to the command.
-		:return:
+		Adds the docstring to the text.
 		"""
-		self.app.add_char_to_text(" \n".join(("precond", "data", "result", "desc", "vars")))
+		self.app.add_char_to_text(" \n".join(self.docstring_components))
+
+	def update_on_syntax_highlight(self, line:str, splitted_line:list, i:int):
+		"""
+		Updates the highlighting of the docstring components.
+		"""
+		pass
 
 
 def init(app):

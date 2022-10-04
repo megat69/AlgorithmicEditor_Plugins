5d0b2eb3d9f466e4ba85a725981e0108b4ae3cd4
megat69 <assassincraft218@gmail.com>
2022-10-04 18:58:59 +0200

    Replaced the app's functions with custom ones

--- insert_mode.py
+++ insert_mode.py
@@ -9,6 +9,8 @@ class InsertModePlugin(Plugin):
 		super().__init__(app)
 		self.insert_mode_enabled = False
 		self.add_command("i", self.toggle_insert_mode, "Insert")
+		self._regular_add_char_to_text = self.app.add_char_to_text
+		self.app.add_char_to_text = self._add_char_to_text
 
 
 	def toggle_insert_mode(self):
@@ -17,6 +19,19 @@ class InsertModePlugin(Plugin):
 		"""
 		self.insert_mode_enabled = not self.insert_mode_enabled
 		self.app.stdscr.addstr(self.app.rows - 1, 4, f"Toggled insert mode to {self.insert_mode_enabled} ")
+
+
+	def _add_char_to_text(self, key:str):
+		"""
+		Adds the given character to the text if the insert mode is disabled, or inserts if enabled.
+		:param key: The character to be inserted.
+		"""
+		if self.insert_mode_enabled is False:
+			self._regular_add_char_to_text(key)
+		else:
+			for character in key:
+				self.app.current_text = self.app.current_text[:self.app.current_index] + character + self.app.current_text[self.app.current_index+1:]
+				self.app.current_index += 1
 
 
 def init(app):

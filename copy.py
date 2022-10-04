4e2d543904f2e8ecde93794e5a68defb6fd0e552
megat69 <assassincraft218@gmail.com>
2022-10-04 21:28:29 +0200

    Added cut

--- copy.py
+++ copy.py
@@ -9,8 +9,10 @@ class CopyPlugin(Plugin):
 	def __init__(self, app):
 		super().__init__(app)
 		self.add_command("co", self.copy, "Copy Selection")
+		self.add_command("x", self.cut, "Cut Selection")
 		self.within_copy = False
 		self.start_index = 0
+		self.do_cut = False
 
 
 	def copy(self):
@@ -19,6 +21,11 @@ class CopyPlugin(Plugin):
 		"""
 		self.start_index = self.app.current_index
 		self.within_copy = True
+		self.do_cut = False
+
+	def cut(self):
+		self.copy()
+		self.do_cut = True
 
 
 	def update_on_keypress(self, key:str):
@@ -38,6 +45,13 @@ class CopyPlugin(Plugin):
 				)
 				# We remember that we are no longer copying
 				self.within_copy = False
+
+				# If we want to cut, we remove all text between the selection areas
+				if self.do_cut:
+					self.app.current_text = self.app.current_text[:min(self.start_index, self.app.current_index)] + \
+						self.app.current_text[max(self.start_index, self.app.current_index):]
+					self.app.current_index -= len(pyperclip.paste())
+
 
 			# Shows a message telling the user that he's still copying
 			else:

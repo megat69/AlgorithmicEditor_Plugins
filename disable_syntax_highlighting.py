5b5cb6ac49d9a8b24110952d91177efbb7762eb3
megat69 <assassincraft218@gmail.com>
2023-10-03 12:37:29 +0200

    Added translations

--- disable_syntax_highlighting.py
+++ disable_syntax_highlighting.py
@@ -4,8 +4,24 @@ from plugin import Plugin
 class DisableSyntaxHighlighting(Plugin):
 	def __init__(self, app):
 		super().__init__(app)
+		self.translations = {
+			"en": {
+				"option_name": "Toggle syntax highlighting",
+				"no_colors": "No colors",
+				"all_colors": "All colors"
+			},
+			"fr": {
+				"option_name": "Activer/Désactiver la coloration syntaxique",
+				"no_colors": "Aucune couleur",
+				"all_colors": "Toutes les couleurs"
+			}
+		}
 		self.state = self.get_config("current_state", True)
-		self.add_option("Toggle syntax highlighting", lambda: self.state, self.toggle)
+		self.add_option(self.translate("option_name"), lambda: (
+			self.translate("no_colors")
+				if self.state else
+			self.translate("all_colors")
+		), self.toggle)
 
 
 	def init(self):

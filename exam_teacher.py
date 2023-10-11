285592596bf2842038e6217abbf25a933fa7c364
megat69 <assassincraft218@gmail.com>
2023-10-11 16:37:30 +0200

    Started work on a plugin pair for exams

---
+++ exam_teacher.py
@@ -0,0 +1,32 @@
+import sys
+import socket
+
+from plugin import Plugin
+
+try:
+	from .stopwatch import StopwatchPlugin
+except ImportError:
+	STOPWATCH_PLUGIN_INSTALLED = False
+	print("You need to install the `stopwatch` plugin to use the exam plugin !")
+else:
+	STOPWATCH_PLUGIN_INSTALLED = True
+
+
+class ExamTeacherPlugin(Plugin):
+	def __init__(self, app):
+		super().__init__(app)
+
+
+class EmptyExamTeacherPlugin(Plugin):
+	"""
+	An empty plugin, so it doesn't affect anybody.
+	"""
+	def __init__(self, app):
+		super().__init__(app)
+
+
+def init(app):
+	if "--examt" in sys.argv and STOPWATCH_PLUGIN_INSTALLED:
+		return ExamTeacherPlugin(app)
+	else:
+		return EmptyExamTeacherPlugin(app)

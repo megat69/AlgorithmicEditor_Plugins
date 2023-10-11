46f733cc0bcfb5f68cc0478b22aed6cd5c1a1d0d
megat69 <assassincraft218@gmail.com>
2023-10-11 16:37:30 +0200

    Started work on a plugin pair for exams

---
+++ exam.md
@@ -0,0 +1,11 @@
+# Exam Plugin - For students
+
+Partake in exams from your teachers !
+
+Features :
+- Automatic stopwatch
+- Automatic submit of your work whenever asked
+- Unobtrusive
+
+## WARNING ! Use command line argument `--exam` to enable !
+Also requires `stopwatch` plugin.
---
+++ exam.py
@@ -0,0 +1,32 @@
+import sys
+import socket
+
+from plugin import Plugin
+
+try:
+	from .stopwatch import StopwatchPlugin
+except ImportError:
+	STOPWATCH_PLUGIN_INSTALLED = False
+	print("You need to install the `stopwatch` plugin to use the exam plugin !")
+else:
+	STOPWATCH_PLUGIN_INSTALLED = True
+
+
+class ExamPlugin(Plugin):
+	def __init__(self, app):
+		super().__init__(app)
+
+
+class EmptyExamPlugin(Plugin):
+	"""
+	An empty plugin, so it doesn't affect anybody.
+	"""
+	def __init__(self, app):
+		super().__init__(app)
+
+
+def init(app):
+	if "--exam" in sys.argv and STOPWATCH_PLUGIN_INSTALLED:
+		return ExamPlugin(app)
+	else:
+		return EmptyExamPlugin(app)

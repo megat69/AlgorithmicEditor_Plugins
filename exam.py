b63fa734fad614434d9058f7e98345c2401c510d
megat69 <assassincraft218@gmail.com>
2023-10-16 01:19:18 +0200

    Server and client can now communicate !

--- exam.py
+++ exam.py
@@ -1,5 +1,6 @@
 import sys
 import socket
+import time
 
 from plugin import Plugin
 from utils import display_menu, input_text
@@ -54,6 +55,9 @@ class ExamPlugin(Plugin):
 		self.client_started = False
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+		# Additional variables
+		self.stopwatch_enabled = False
+
 
 	def init(self):
 		# Inits the Stopwatch plugin
@@ -64,6 +68,20 @@ class ExamPlugin(Plugin):
 		# Connects to the server
 		# TODO: solid menu
 		self.connect_to_server()
+		print(f"Connected to server {self.server_ip} !")
+
+		# Receives the stopwatch information from the server
+		temp_stopwatch_info = self.receive_information()
+		print(f"Stopwatch info is now {temp_stopwatch_info}")
+		# Analyzes the stopwatch information to get the value of the stopwatch
+		# The stopwatch information is a string like the following example : "01:23:45"
+		stopwatch_value = [int(e) for e in temp_stopwatch_info.split(':')]
+		# Sets the stopwatch value to be as such, and currently inactive
+		self.stopwatch_plugin.stopwatch_value = stopwatch_value
+		self.stopwatch_plugin.enabled = True
+
+		# Removes input control from the user
+		self.app.input_locked = True
 
 
 	def connect_to_server(self):
@@ -89,6 +107,38 @@ class ExamPlugin(Plugin):
 				self.app.stdscr.getch()
 			else:
 				retry = False
+
+
+	def receive_information(self) -> str:
+		"""
+		Receives the amount of information required from the server.
+			NOTE: It is recommended to run this function in a dedicated thread.
+		:return: The data from the server as a string.
+		"""
+		# Reads a first two bytes of information : these contain the size of the message sent by the server
+		message_size_bytes = self.socket.recv(2)
+		message_size = int(message_size_bytes.decode("utf-8"))
+
+		# Now reads from the server the amount of data to be received
+		server_data = self.socket.recv(message_size)
+
+		# Decodes the data into a utf-8 string
+		decoded_data = server_data.decode("utf-8")
+
+		# Returns the data
+		return decoded_data
+
+
+	def fixed_update(self):
+		# While the clock stopwatch is disabled, we reset its time
+		# self.stopwatch_plugin.fixed_update()
+		if self.stopwatch_enabled is False:
+			self.stopwatch_plugin.start_time = int(time.time())
+			self.stopwatch_plugin.end_time = (
+				int(time.time()) + self.stopwatch_plugin.stopwatch_value[2] +
+				self.stopwatch_plugin.stopwatch_value[1] * 60 +
+				self.stopwatch_plugin.stopwatch_value[0] * 3600
+			)
 
 
 class EmptyExamPlugin(Plugin):

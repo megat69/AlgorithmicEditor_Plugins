2e82dc76cb2e2c2bb41ecd34f5471a5d861c0935
megat69 <assassincraft218@gmail.com>
2023-10-16 01:18:59 +0200

    Class is now a singleton

--- stopwatch.py
+++ stopwatch.py
@@ -10,6 +10,16 @@ class StopwatchPlugin(Plugin):
 	PERCENTAGE_LOW = 0.25
 	PERCENTAGE_VERY_LOW = 0.1
 
+	__singleton = None
+
+	def __new__(cls, *args, **kwargs):
+		"""
+		Creates a singleton of the class.
+		"""
+		if cls.__singleton is None:
+			cls.__singleton = super().__new__(cls)
+		return cls.__singleton
+
 
 	def __init__(self, app):
 		super().__init__(app)
